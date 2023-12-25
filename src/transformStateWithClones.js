d53a045441007800ecf27c2df35698b0f03e69f0
ALion Alex <alionalexxx@gamil.com>
2023-12-25 19:43:28 +0200

    completed tsk

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let currentState = { ...state };
+
+  const result = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        Object.assign(currentState, action.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const keyToRemove of action.keysToRemove) {
+          delete currentState[keyToRemove];
+        }
+        break;
+
+      case 'clear':
+        currentState = {};
+        break;
+    }
+    result.push({ ...currentState });
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
